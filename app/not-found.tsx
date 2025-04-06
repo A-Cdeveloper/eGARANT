a169cd431b511276c194ae53f58e6955710cfa0c
Aleksandar <aleksandar@e-vlasotince.info>
2025-04-06 17:11:14 +0200

    change layout in no-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,16 +9,16 @@ export default function NotFound() {
   const router = useRouter();
 
   return (
-    <div className="w-full h-full flex justify-center items-center text-center">
+    <div className="w-full h-[40vh] flex justify-center items-center text-center">
       <div>
         <Image
           src={errorImg}
           alt="404"
-          width={100}
+          width={95}
           height={100}
           className="mx-auto"
         />
-        <h2 className="my-5 text-[26px]">Stranica nije pronađena!</h2>
+        <h2 className="my-5 text-[20px]">Stranica nije pronađena!</h2>
 
         <Button size="lg" variant="primary" onClick={() => router.push("/")}>
           Povratak na početnu
