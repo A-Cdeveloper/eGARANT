05468ad91064ea2073b23e99dd5b207c5722c690
Aleksandar <aleksandar@e-vlasotince.info>
2025-04-06 17:11:01 +0200

    create server actions to get invoices/invoice

--- actions/invoices/index.ts
+++ actions/invoices/index.ts
@@ -1,27 +1,70 @@
 "use server";
 
 import prisma from "@/lib/db";
+import { parseError } from "@/lib/errors";
 import { InvoiceWithSeller } from "@/types";
+
+//const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const getAllUserInvoices = async (
   uid: string
-): Promise<InvoiceWithSeller[]> => {
-  const invoices = await prisma.invoice.findMany({
-    where: {
-      user: {
-        uid,
-      },
-    },
-    include: {
-      seller: {
-        select: {
-          name: true,
-          address: true,
-          city: true,
+): Promise<
+  { data: InvoiceWithSeller[]; error: null } | { data: null; error: string }
+> => {
+  try {
+    const invoices = await prisma.invoice.findMany({
+      where: {
+        user: {
+          uid,
         },
       },
-    },
-  });
+      include: {
+        seller: {
+          select: {
+            name: true,
+            address: true,
+            city: true,
+          },
+        },
+      },
+    });
 
-  return invoices;
+    return { data: invoices, error: null };
+  } catch (error) {
+    return { data: null, error: parseError(error) };
+  }
+};
+
+////////////////////////////////////////////////////////////////////
+export const getUserInvoice = async (
+  iid: string,
+  uid: string
+): Promise<
+  { data: InvoiceWithSeller; error: null } | { data: null; error: string }
+> => {
+  try {
+    const invoice = await prisma.invoice.findUnique({
+      where: {
+        iid,
+        uid,
+      },
+      include: {
+        seller: {
+          select: {
+            name: true,
+            address: true,
+            city: true,
+          },
+        },
+      },
+    });
+
+    if (!invoice) {
+      return { data: null, error: "Račun ne postoji." };
+    }
+
+    return { data: invoice, error: null };
+  } catch (error) {
+    return { data: null, error: parseError(error) };
+  }
 };
