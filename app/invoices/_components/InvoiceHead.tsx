b490e02c89fac3d331c9885141812f4726a1e25a
Aleksandar <aleksandar@e-vlasotince.info>
2025-04-10 00:03:32 +0200

    useMemo calculate total invoice price

--- app/invoices/_components/InvoiceHead.tsx
+++ app/invoices/_components/InvoiceHead.tsx
@@ -7,14 +7,16 @@ import {
 import { InvoiceWithSeller, Product } from "@/types";
 import { ExternalLink } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import React, { useMemo } from "react";
 
 const InvoiceHead = ({ invoice }: { invoice: InvoiceWithSeller }) => {
-  const total = reduceJsonArray<Product>(
-    invoice?.products,
-    (acc, p) => acc + p.unit_price * p.quantity
-  );
-
+  const total = useMemo(() => {
+    return reduceJsonArray<Product>(
+      invoice?.products,
+      (acc, p) => acc + p.unit_price * p.quantity,
+      0
+    );
+  }, [invoice?.products]);
   return (
     <div className="border-y-2 border-primary/10 bg-white p-4 flex flex-wrap text-[15px] justify-beatween">
       <div className="w-full sm:w-1/2 mb-4 sm:mb-0">
