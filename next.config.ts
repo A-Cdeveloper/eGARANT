bc83f1e6d51b31607dd4d0a299a4d841e44e1f02
Aleksandar <aleksandar@e-vlasotince.info>
2025-04-09 02:14:53 +0200

    add pinata host image

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "picsum.photos",
       },
+      {
+        protocol: "https",
+        hostname: "moccasin-obedient-guanaco-455.mypinata.cloud",
+      },
     ],
   },
 };
