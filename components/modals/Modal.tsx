68fb78d791304cd9182a795aae4ad745ea6e161a
Aleksandar <aleksandar@e-vlasotince.info>
2025-04-11 10:41:52 +0200

    style modal

--- components/modals/Modal.tsx
+++ components/modals/Modal.tsx
@@ -24,9 +24,11 @@ const Modal = ({ onClose, onConfirm, message }: ModalType) => {
       className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-primary/50 z-500"
       onClick={onClose}
     >
-      <div className="bg-white text-primary w-1/4 max-w-[250px] h-auto flex flex-col justify-center items-center p-5 rounded-md">
-        <h3>{message}</h3>
-        <div className="flex gap-x-2 mt-4">
+      <div className="bg-white text-primary w-1/4 max-w-[250px] h-auto flex flex-col justify-center items-center rounded-md">
+        <h3 className="w-full text-center py-2 border-b border-gray-300">
+          {message}
+        </h3>
+        <div className="flex gap-x-2 mt-4 pb-5">
           <Button variant="primary" onClick={onClose}>
             Odustani
           </Button>
