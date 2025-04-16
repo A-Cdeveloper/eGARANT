eafecffd0afdec8b6c9c08335ac71b53aaed2d15
Aleksandar <aleksandar@e-vlasotince.info>
2025-04-16 16:17:51 +0200

    zustand store for auth

---
+++ store/index.ts
@@ -0,0 +1,32 @@
+import { UserResponseType, getUserFromCookies } from "@/actions/auth";
+
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+type AuthStoreType = {
+  user: UserResponseType | null;
+  tokenExpiry: number | null;
+  isAuth: boolean;
+  setUser: () => Promise<void>;
+  removeUser: () => void;
+  //   setTokenExpiry: (tokenExpiry: number | null) => void;
+};
+
+export const useAuthStore = create<AuthStoreType>()(
+  persist(
+    (set) => ({
+      user: null,
+      tokenExpiry: null,
+      isAuth: false,
+      setUser: async () => {
+        const { data: user, tokenExpiry } = await getUserFromCookies();
+        set({ user, tokenExpiry, isAuth: !!user });
+      },
+      removeUser: () => set({ user: null, tokenExpiry: null, isAuth: false }),
+    }),
+    {
+      name: "auth-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
