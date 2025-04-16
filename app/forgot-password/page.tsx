52a963c078c0aa46b10615b172fb44b237fe6735
Aleksandar <aleksandar@e-vlasotince.info>
2025-04-16 02:20:25 +0200

    forgotPassword page

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ForgotPasswordForm from "./_components/ForgotPasswordForm";
+
+const ForgotPasswordPage = () => {
+  return (
+    <div className="text-center">
+      <ForgotPasswordForm />
+    </div>
+  );
+};
+
+export default ForgotPasswordPage;
