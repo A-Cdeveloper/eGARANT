9bdf9fb301bf6e4171f1476b2f9ed1089c038d4b
Aleksandar <aleksandar@e-vlasotince.info>
2025-04-16 16:17:28 +0200

    add Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,7 @@
-import { getUserFromCookies } from "@/actions/auth";
+import { authSecurityPatch } from "@/lib/authSecurityPatch";
 
 export default async function Home() {
-  const user = await getUserFromCookies();
+  await authSecurityPatch();
 
-  console.log(user);
-
-  return (
-    <>
-      <h2>Dobrodosli Aleksandar Cvetkovic</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet ab
-        quaerat quos deleniti corporis libero repudiandae quo voluptatum
-        corrupti voluptate, quod facere dolorum, odit nihil aliquid? Labore odit
-        omnis aliquid.
-      </p>{" "}
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet ab
-        quaerat quos deleniti corporis libero repudiandae quo voluptatum
-        corrupti voluptate, quod facere dolorum, odit nihil aliquid? Labore odit
-        omnis aliquid.
-      </p>{" "}
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet ab
-        quaerat quos deleniti corporis libero repudiandae quo voluptatum
-        corrupti voluptate, quod facere dolorum, odit nihil aliquid? Labore odit
-        omnis aliquid.
-      </p>
-    </>
-  );
+  return <div>Dashboard</div>;
 }
